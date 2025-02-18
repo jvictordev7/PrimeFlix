0d97dbb0fa6d82404182004e1a280f3bca4c5c33
João Victor <156962782+jvictordev7@users.noreply.github.com>
2025-02-18 10:37:51 -0300

    Listando filmes

--- primeflix/src/pages/Home/index.js
+++ primeflix/src/pages/Home/index.js
@@ -1,5 +1,7 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 function Home() {
 
@@ -16,17 +18,28 @@ useEffect(()=>{
       }
     });
 
-    console.log(response.data.results);
+    setFilmes(response.data.results.slice(0, 10));
 
   }
 
   loadFilmes();
 
-}, {})
+}, [])
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+              
+            </article>
+          )
+        })}
+      </div>
     </div>
   );
 }
